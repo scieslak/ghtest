4975f4f33e6342425ca9a35fe7b8ed2742410606
scieslak <scieslak@scieslak.com>
2017-01-21 22:54:12 +0000

    Update initializer https

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-ApplicationController.renderer.defaults.merge!(
-  http_host: 'ghtest.scieslak.com',
-  https: true
-)
+# ApplicationController.renderer.defaults.merge!(
+#   http_host: 'ghtest.scieslak.com',
+#   https: true
+# )
